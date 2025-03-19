c9b1c90a6636f630db51fadaf7be02735e80cd57
PranamB21 <pranamb059@gmail.com>
2025-03-20 00:16:22 +0530

    1.7

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="skin_tone_analyzer",
     version="1.0.0",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'flask==2.0.1',
         'Werkzeug==2.0.3',
