1cc6de828304a9c2467cf69e41a9bed834fdffb7
PranamB21 <pranamb059@gmail.com>
2025-03-23 20:44:39 +0530

    2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="skin_tone_analyzer",
     version="1.0.0",
-    packages=find_packages(include=['skin_tone_analyzer', 'skin_tone_analyzer.*']),
+    packages=find_packages(),
     include_package_data=True,
     package_data={
         'skin_tone_analyzer': ['*.py'],
@@ -17,4 +17,5 @@ setup(
         'opencv-python-headless==4.8.0.76',
         'Pillow==9.5.0'
     ],
+    python_requires='>=3.11',
 ) 
--- skin_tone_analyzer/__init__.py
+++ skin_tone_analyzer/__init__.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# Add the parent directory to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from .skin_tone import process_image, detect_skin_tone, recommend_colors, create_color_preview
 
 __all__ = ['process_image', 'detect_skin_tone', 'recommend_colors', 'create_color_preview'] 
